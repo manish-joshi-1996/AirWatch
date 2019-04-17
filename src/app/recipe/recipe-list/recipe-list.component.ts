c465f452dc1958ff16bdd7c5ab667a7e92191c0e
manish-joshi-1996 <45786748+manish-joshi-1996@users.noreply.github.com>
2019-04-17 23:44:46 +0530

    Till shopping list

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('A test Recipe','Just a test','https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*')
+    
+  ];
 
   constructor() { }
 
