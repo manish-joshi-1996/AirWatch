b89175844c5cfb86afa7757b72dec8f36c33fa60
manish-joshi-1996 <45786748+manish-joshi-1996@users.noreply.github.com>
2019-04-30 00:05:23 +0530

    all navigation functionality working

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+ @ViewChild('nameinput') nameinputRef:ElementRef;
+ @ViewChild('amountinput') amountinputRef:ElementRef;
 
+ @Output() ingredientadded=new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+  onAddItem(){
+    const ingname=this.nameinputRef.nativeElement.value;
+    const ingamount=this.amountinputRef.nativeElement.value;
+    const newIngredient=new Ingredient(ingname,ingamount);
+    this.ingredientadded.emit(newIngredient);
   }
 
 }
